81181a8129053f459291cd89750b8417e27f6ad1
salunkhep12 <152468288+salunkhep12@users.noreply.github.com>
2024-09-30 09:42:40 +0530

    add new files

---
+++ script.js
@@ -0,0 +1,15 @@
+gsap.to("#box1", {
+    x: 800,
+    duration: 3,
+    rotate: 360,
+    backgroundColor: "#fff",
+    borderRadius: "50%"
+})
+
+gsap.to("#box2", {
+    x: 800,
+    duration: 3,
+    rotate: 360,
+    backgroundColor: "#fff",
+    borderRadius: "50%"
+})
